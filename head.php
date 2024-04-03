b4a0341a31fb0e598dd277e4092abe6367df066e
Dobri Dobrev <dobri_dobrev@yahoo.com>
2024-04-03 21:24:01 +0200

    I delete some files like maps of css and minify the main css file

--- head.php
+++ head.php
@@ -9,7 +9,7 @@ $navigationJson = json_decode(file_get_contents('./navigation.json'), true); // 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= $headDescription ?>">  <!-- i set a variable to set different description for each page (Seo purpose)  -->
-    <link rel="stylesheet" href="./sass/style.css">
+    <link rel="stylesheet" href="./sass/style.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
